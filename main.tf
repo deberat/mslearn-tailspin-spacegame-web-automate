79416e447564434ced64224fc8e2977c93afa996
Denis BERAT <deberat@microsoft.com>
2020-05-22 14:08:49 +0200

    Add azurerm provider section with feature in main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ terraform {
   }
 }
 
+provider azurerm {
+  feature {}
+}
+
 variable "resource_group_name" {
   default = "tailspin-space-game-rg"
   description = "The name of the resource group"
